f627f81dd257cfa3c3f0b14e051235a62771059d
Eloy <eloydgutierrez@gmail.com>
2020-10-08 20:20:33 -0400

    improved code

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -17,5 +17,5 @@ for a in q:
     for b in q:
         for c in q:
             for d in q:
-                if f(a) + f(b) == f(c) - f(d):
+                if (f(a) + f(b)) == (f(c) - f(d)):
                     print(f'f({a}) + f({b}) = f({c}) - f({d})  {f(a)} + {f(b)} = {f(c)} - {f(d)}')
